8151d09b54786a0a4bab8e2e1928296b899140ab
PM <pitimor@o2.pl>
2017-02-20 00:16:00 +0100

    post timestamp fix

--- backend/views/post/index.php
+++ backend/views/post/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'name',
             'content:ntext',
-            'created_at',
+            'created_at:datetime',
             'created_by',
             // 'updated_at',
             // 'updated_by',
