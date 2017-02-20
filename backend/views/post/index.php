b30b12c5cd4535b76f433a77acd2436cdff3f89a
P M <P M>
2017-02-20 11:43:12 +0100

    post user id

--- backend/views/post/index.php
+++ backend/views/post/index.php
@@ -32,6 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'updated_at:datetime',
 			[
 				'class' => 'yii\grid\DataColumn',
+				'attribute' => 'created_by',
 				'value' => function($data)
 				{
 					return User::find()->where(['id' => $data->created_by])->one()->username;
